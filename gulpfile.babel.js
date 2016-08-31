0556c678907aa81c4116881115b55e3005639d0e
zanjs <root@zanjs.com>
2016-08-31 16:22:26 +0800

    Energy and persistence conquer all things. :umbrella:

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,7 +5,7 @@
  * 2016年06月12日16:44:43
  */
 import pkg          from './package.json';
-
+import conf         from './config.json';
 import gulp         from 'gulp';
 import sass         from 'gulp-sass';
 import concat       from 'gulp-concat';
@@ -17,12 +17,29 @@ import imagemin     from 'gulp-imagemin';
 import header       from 'gulp-header';
 import autoprefixer from 'gulp-autoprefixer';
 import px2rem       from 'gulp-pxrem';
+import ejs          from 'gulp-ejs';
 
 
 
-const day = '20160809', mincss = 'app.css', minjs = 'app.js',desc = '拼邮分享H5';
+const day           = conf.start;
+const title         = conf[day].title;
+const description   = conf[day].description;
+const keywords      = conf[day].keywords;
+const author        = conf[day].author;
+const version       = conf[day].version;
+const mincss        = conf[day].build.css;
+const minjs         = conf[day].build.js;
 
-import conf        from `${day}/config.json`;
+
+
+let cssLoadSrc = conf[day].load.css;
+let jsLoadSrc  = conf[day].load.js;
+let csssrc = `./${day}/src/scss/main.scss`;
+let jssrc = `./${day}/src/js/*.js`;
+
+
+console.log(cssLoadSrc)
+console.log(jsLoadSrc)
 
 const browserSync = require('browser-sync').create();
 const reload      = browserSync.reload;
@@ -30,25 +47,41 @@ const reload      = browserSync.reload;
 const banner = [
   '/*! ',
     '<%= pkg.name %> ',
-    'v<%= pkg.version %> | ',
-    `(c) ${new Date()} <%= pkg.author %> |`,
+    `v ${version}  | `,
+    `(c) ${new Date()}  ${author}  |`,
     ' <%= pkg.homepage %> ',
-    ' <%= pkg.description %>',
+    ` ${title}`,
   ' */',
   '\n'
 ].join('');
 
-//编译Sass，Autoprefix及缩小化
-gulp.task('sass', () => gulp.src(`./${day}/src/scss/main.scss`)
+
+
+gulp.task('ejs', () => gulp.src(`./${day}/src/templates/*.ejs`)
+    .pipe(ejs({
+        title: title,
+        description: description,
+        keywords: keywords,
+        mincss: mincss,
+        minjs: minjs,
+        time: new Date().getTime()
+    }))
+    .pipe(gulp.dest(`./${day}/.__`))
+    .pipe(rename({extname: ".html"}))
+    .pipe(gulp.dest(`./${day}/`))
+    .pipe(reload({ stream: true }))
+    .pipe(notify({ message: 'ejs task complete' })))
+
+gulp.task('sass', () => gulp.src(cssLoadSrc)
     .pipe(sass({ style: 'expanded' }))
-    .pipe(gulp.dest(`./${day}/.tmp/css`))
+    .pipe(gulp.dest(`./${day}/.__/css`))
     .pipe(autoprefixer({
 			browsers: ['> 1%', 'IE 8','Firefox >= 20'],
 			cascade: false
 	 }))
     .pipe(px2rem())
     .pipe(rename(mincss))
-    .pipe(gulp.dest(`./${day}/.tmp/`))
+    .pipe(gulp.dest(`./${day}/.__/`))
     .pipe(minifycss())
     
     .pipe(header(banner, { pkg }))
@@ -58,9 +91,9 @@ gulp.task('sass', () => gulp.src(`./${day}/src/scss/main.scss`)
 
 
 
-gulp.task('scripts',() => gulp.src(`./${day}/src/js/*.js`)
+gulp.task('scripts',() => gulp.src(jsLoadSrc)
     .pipe(concat(minjs))
-    .pipe(gulp.dest(`./${day}/.tmp/js`))
+    .pipe(gulp.dest(`./${day}/.__/js`))
     .pipe(uglify())
     .pipe(header(banner, { pkg }))
     .pipe(gulp.dest(`./${day}/build/js/`))
@@ -80,8 +113,8 @@ gulp.task('html',() => {
         .pipe(reload({stream: true}))
 });
 
-    // 静态服务器 + 监听 scss/html 文件
-gulp.task('dev', ['sass','scripts'], () => {
+// 静态服务器 + 监听 scss/html 文件
+gulp.task('dev', ['sass','scripts','ejs'], () => {
 
     browserSync.init({
         server: `./${day}/`
@@ -96,8 +129,10 @@ gulp.task('dev', ['sass','scripts'], () => {
     gulp.watch(`./${day}/src/images/*`, ['images']);
 
     // 看守所有.html
-    gulp.watch(`./${day}/*.html`).on('change', reload);;
-    gulp.watch('./*.html').on('change', reload);;
+    gulp.watch(`./${day}/*.html`).on('change', reload);
+    gulp.watch('./*.html').on('change', reload);
+
+    gulp.watch([`./${day}/src/templates/*.ejs` ,`./${day}/src/templates/**/*.html`], ['ejs']);
 
 });
 
